4164bfd28db9d02f139e56247ac960caed9f55d4
Danila <EL_dranick@mail.ru>
2020-10-15 10:37:35 +0300

    +

---
+++ src/homework3.java
@@ -0,0 +1,56 @@
+public class homework3 {
+    public static void main(String[] args) {
+        int month = 6;
+        if (month <= 2 | month==12) {
+            System.out.println("Зима");
+        } else if(month < 6) {
+            System.out.println("Весна");
+        } else if (month < 9) {
+            System.out.println("Лето");
+        } else if (month <= 11) {
+            System.out.println("Осень");
+        }
+
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+        }
+
+        int b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for(int i = 9; i < b.length & i > -1; i--) {
+            System.out.print(b[i] + " ");
+        }
+        System.out.println();
+        int squareArray[][] = new int[2][3];
+        squareArray[0][0] = 1;
+        squareArray[0][1] = 2;
+        squareArray[0][2] = 3;
+        squareArray[1][0] = 4;
+        squareArray[1][1] = 5;
+        squareArray[1][2] = 6;
+        for(int i = 0; i < squareArray.length; i++){
+            for (int j = 0; j < squareArray[i].length; j++){
+                System.out.print(squareArray[i][j] + "[" + i + "]" + "[" + j + "] ");
+            }
+            System.out.println();
+        }
+    }
+}
